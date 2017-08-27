870d67899a1ced3d176ccb75f38e28323680bed8
Chih-Hao <zhihaozhang@me.com>
2017-08-27 15:17:30 +0800

    Initial Commit

---
+++ plugAndPlay/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  plugAndPlay
+//
+//  Created by Chih-Hao on 2017/8/27.
+//  Copyright © 2017年 Chih-Hao. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
